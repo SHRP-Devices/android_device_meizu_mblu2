ada995d43909bd423c325e5f909510aa47de8039
GuilianoS <guiliano-sfalcin@hotmail.com>
2020-11-16 16:24:54 -0300

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,4 +1,4 @@
 #If you want to make x32 version of shrp just make this option true
 m253_32_BUILD := true
 
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/omni_mblu2.mk
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/omni_m253.mk
