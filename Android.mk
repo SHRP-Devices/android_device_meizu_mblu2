ce3d6e43d457afee37ab12c7cd2272cff56dedf9
GuilianoS <guiliano-sfalcin@hotmail.com>
2020-11-16 16:22:05 -0300

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 LOCAL_PATH := $(call my-dir)
-ifneq ($(filter mblu2,$(TARGET_DEVICE)),)
+ifneq ($(filter m253,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 include $(CLEAR_VARS)
 BOARD_RECOVERY_IMAGE_PREPARE := $(LOCAL_PATH)/recovery/recovery_inject.sh "$(TARGET_RECOVERY_ROOT_OUT)"
